9bf64631e3b9f2352710fc61ab8b6aa7c5667cbb
Abu Huraira <iamhuraira429@gmail.com>
2023-01-02 18:32:59 +0500

    Creating Floating div and blur div

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -5,10 +5,11 @@ import Linkedin from '../../img/linkedin.png';
 import Instagram from '../../img/instagram.png';
 import vector1 from '../../img/Vector1.png';
 import vector2 from '../../img/Vector2.png';
-import boy from '../../img/boy.png';
+import boy from '../../img/boy1.png';
 import thumbup from '../../img/thumbup.png';
 import Crown from '../../img/crown.png';
 import glassesimoji from '../../img/glassesimoji.png';
+import FloatingDiv from '../FloatingDiv/FloatingDiv';
 
 const Intro = () => {
   return (
@@ -30,6 +31,16 @@ const Intro = () => {
               <img src={vector1} alt="" />
               <img src={vector2} alt="" />
               <img src={boy} alt="" />
+              <img src={glassesimoji} alt="" />
+              <div style={{top: '-1%', left:'65%'}}>
+                  <FloatingDiv image={Crown} txt1={'MERN'} txt2={'Developer'} />
+              </div>
+              <div style={{ top: '17rem', left: '0' }}>
+                  <FloatingDiv image={thumbup} txt1={'Raising Talent'} txt2={'Award'} />
+              </div>
+              {/* Blur Div */}
+              <div className="blur" style={{ background: "rgb(238 210 255)" }}> </div>
+              <div className="blur" style={{ background: "#C1f5ff", top: '17rem', width:'22rem' , height: '11rem', left : '-9rem'   }}> </div>
          </div>
     </div>
   )
